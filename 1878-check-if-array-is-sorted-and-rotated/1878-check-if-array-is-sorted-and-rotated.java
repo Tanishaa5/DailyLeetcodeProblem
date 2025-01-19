c325752690c68865d987d36a2b404d5fc74ed2c1
Tanishaa5 <121441180+Tanishaa5@users.noreply.github.com>
2025-01-19 22:03:25 +0530

    Time: 0 ms (100%), Space: 41.2 MB (77.97%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
@@ -2,19 +2,17 @@ class Solution {
     public boolean check(int[] nums) {
         int n = nums.length;
         int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            // Compare current element with the next (using modulo for circular comparison)
-            if (nums[i] > nums[(i + 1) % n]) {
+        if (nums[0] < nums[n - 1]) {
+            count++;
+        }
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
                 count++;
             }
-            // If more than one descending break, return false
             if (count > 1) {
                 return false;
             }
         }
-
-        // Array is sorted and rotated if there's at most one descending break
         return true;
     }
 }
