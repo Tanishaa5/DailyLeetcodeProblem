f9304d22144f0c66ecd21b163fe5e03f8cf68c87
Tanishaa5 <121441180+Tanishaa5@users.noreply.github.com>
2025-01-23 10:35:28 +0530

    Time: 1 ms (99.9%), Space: 46.4 MB (25.81%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -2,7 +2,7 @@ class Solution {
     public int singleNumber(int[] nums) {
         int ans=0;
         for(int i=0;i<nums.length;i++){
-            ans=ans^nums[i];
+            ans=ans^nums[i];//sare same number ko vo 0 kr dega and answer me bas 1 rhega 
         }
         return ans;
         
