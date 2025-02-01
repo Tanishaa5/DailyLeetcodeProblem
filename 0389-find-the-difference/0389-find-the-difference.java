c52bb10c62ee88195974154cf6f50b833c70c977
Tanishaa5 <121441180+Tanishaa5@users.noreply.github.com>
2025-02-01 19:31:21 +0530

    Time: 12 ms (11.04%), Space: 42.1 MB (18.82%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,20 +1,43 @@
+// class Solution {
+//     public char findTheDifference(String s, String t) {
+//         int[] hash = new int[26];
+
+//         // Step 1: Count frequency in s
+//         for (char c : s.toCharArray()) {
+//             hash[c - 'a']++;
+//         }
+
+//         // Step 2: Decrease frequency for t and find the extra character
+//         for (char c : t.toCharArray()) {
+//             hash[c - 'a']--;  
+//             if (hash[c - 'a'] < 0) { // The extra character will have a negative frequency
+//                 return c;
+//             }
+//         }
+        
+//         return ' '; // This will never be reached
+//     }
+// }
+//Hashmap
 class Solution {
     public char findTheDifference(String s, String t) {
-        int[] hash = new int[26];
+        HashMap<Character,Integer>map=new HashMap<>();
+        for(char c:s.toCharArray()){
+            if(map.containsKey(c)){
+                map.put(c,map.get(c)+1);
 
-        // Step 1: Count frequency in s
-        for (char c : s.toCharArray()) {
-            hash[c - 'a']++;
-        }
 
-        // Step 2: Decrease frequency for t and find the extra character
-        for (char c : t.toCharArray()) {
-            hash[c - 'a']--;  
-            if (hash[c - 'a'] < 0) { // The extra character will have a negative frequency
-                return c;
+            }else{
+                map.put(c,1);
             }
         }
-        
-        return ' '; // This will never be reached
-    }
-}
+        for(char c:t.toCharArray()){
+            if (!map.containsKey(c) || map.get(c) == 0) {
+                return c;  // This is the extra character
+            } else {
+                map.put(c, map.get(c) - 1);  // Decrement the count as we find a match
+            }
+        }
+        return ' ';
+    }}
+
