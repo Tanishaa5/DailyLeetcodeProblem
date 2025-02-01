9e25595d30334541d4657885cd138f8a2ab85f65
Tanishaa5 <121441180+Tanishaa5@users.noreply.github.com>
2025-02-01 19:14:55 +0530

    Time: 3 ms (89.37%), Space: 43.4 MB (45.63%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,25 +1,56 @@
+// class Solution {
+//     public int[] intersect(int[] nums1, int[] nums2) {
+//         int []hash=new int[1001];
+//         for(int num:nums1){
+//             hash[num]++;
+//         }
+//         ArrayList <Integer> result=new ArrayList<>();
+//         for(int num:nums2){
+//             if(hash[num]>0){
+//                 result.add(num);
+//                 hash[num]--;
+                
+//             }
+//         }
+//         int arr[]=new int[result.size()];
+//         for(int i=0;i<result.size();i++){
+//             arr[i]=result.get(i);
+//         }
+//         return arr;
+
+
+
+        
+//     }
+// }
+
+
+//hashmap
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        int []hash=new int[1001];
-        for(int num:nums1){
-            hash[num]++;
-        }
-        ArrayList <Integer> result=new ArrayList<>();
-        for(int num:nums2){
-            if(hash[num]>0){
-                result.add(num);
-                hash[num]--;
-                
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int num: nums1){
+
+            if(map.containsKey(num)){
+                map.put(num,map.get(num)+1);//count or frequency increment
+            }
+            else{
+                map.put(num,1);
             }
         }
-        int arr[]=new int[result.size()];
+        ArrayList<Integer>result=new ArrayList<>();
+
+        for(int num:nums2){
+            if(map.containsKey(num) && map.get(num)>0){
+                result.add(num);
+                map.put(num,map.get(num)-1);
+            }
+        }
+         int arr[]=new int[result.size()];
         for(int i=0;i<result.size();i++){
             arr[i]=result.get(i);
         }
         return arr;
 
 
-
-        
-    }
-}
+    }}
