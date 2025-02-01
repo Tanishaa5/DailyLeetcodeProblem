ca6b85da43e35a776c269aa962ba400fcf63d6e2
Tanishaa5 <121441180+Tanishaa5@users.noreply.github.com>
2025-02-01 19:02:42 +0530

    Time: 3 ms (56%), Space: 43.4 MB (48.49%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,21 +1,40 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        int []hash=new int[1001];
+        // int []hash=new int[1001];
+        // for(int num:nums1){
+        //     hash[num]=1;//mark the presence
+        // }
+        // ArrayList <Integer> result=new ArrayList<>();
+        // for(int num:nums2){
+        //     if(hash[num]==1){//check if second array element also exist in first array
+        //         result.add(num);
+        //         hash[num]=0;//to avoid duplicate
+        //     }
+        // }
+        // int arr[]=new int[result.size()];
+        // for(int i=0;i<result.size();i++){
+        //     arr[i]=result.get(i);
+        // }
+        // return arr;
+
+        HashMap<Integer,Integer> map=new HashMap<>();
         for(int num:nums1){
-            hash[num]=1;//mark the presence
+            map.put(num,1);//and if it is already present in hashmap then it won't do anything
         }
-        ArrayList <Integer> result=new ArrayList<>();
+        ArrayList<Integer>result=new ArrayList<>();
         for(int num:nums2){
-            if(hash[num]==1){//check if second array element also exist in first array
+            if(map.containsKey(num) && map.get(num)==1){
                 result.add(num);
-                hash[num]=0;//to avoid duplicate
+               map.put(num, map.get(num) - 1);
+
             }
         }
-        int arr[]=new int[result.size()];
+        int []ans=new int[result.size()];
         for(int i=0;i<result.size();i++){
-            arr[i]=result.get(i);
+            ans[i]=result.get(i);
         }
-        return arr;
+        return ans;
+
 
 
 
