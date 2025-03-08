c02b771d700a6864a0e9796f733514151f3d5c44
Tanishaa5 <121441180+Tanishaa5@users.noreply.github.com>
2025-03-08 19:19:12 +0530

    Time: 0 ms (100%), Space: 45.8 MB (44.98%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,22 +1,47 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start=0;
-        int end=nums.length-1;
-        while(start<=end){
-            int mid=(start+end)/2;
+        return recursiveBinary(nums, 0, nums.length - 1, target);
+    }
 
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[mid]>target){
-                end=mid-1;
-            }
-            else{
-                start=mid+1;
-            }
+    public int recursiveBinary(int[] nums, int start, int end, int target) {
+        // Base case: If the search space is exhausted
+        if (start > end) {
+            return -1;
         }
-        return -1;
 
-        
+        int mid = start + (end - start) / 2; // To prevent integer overflow
+
+        if (nums[mid] == target) {
+            return mid; // Target found
+        } else if (nums[mid] > target) {
+            return recursiveBinary(nums, start, mid - 1, target); // Search left half
+        } else {
+            return recursiveBinary(nums, mid + 1, end, target); // Search right half
+        }
     }
 }
+
+
+
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         int start=0;
+//         int end=nums.length-1;
+//         while(start<=end){
+//             int mid=(start+end)/2;
+
+//             if(nums[mid]==target){
+//                 return mid;
+//             }
+//             else if(nums[mid]>target){
+//                 end=mid-1;
+//             }
+//             else{
+//                 start=mid+1;
+//             }
+//         }
+//         return -1;
+
+        
+//     }
+// }
