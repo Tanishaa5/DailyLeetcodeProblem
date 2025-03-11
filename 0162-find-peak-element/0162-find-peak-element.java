3eed0bac25faded0b0554c2ddff4e8cf7ba25942
Tanishaa5 <121441180+Tanishaa5@users.noreply.github.com>
2025-03-11 19:35:01 +0530

    Time: 0 ms (100%), Space: 42.6 MB (42.24%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,21 +1,17 @@
-class Solution {
-    public int findPeakElement(int[] arr) {
-        int s = 0;
-        int e = arr.length - 1;
+class Solution{
+    public int findPeakElement(int[] nums) {
+    int left = 0, right = nums.length - 1;
+    
+    while (left < right) {
+        int mid = left + (right - left) / 2;
 
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-
-            // Handle edge cases and general case:
-            if ((mid == 0 || arr[mid] > arr[mid - 1]) && 
-                (mid == arr.length - 1 || arr[mid] > arr[mid + 1])) {
-                return mid; // Found a peak
-            } else if (mid > 0 && arr[mid] < arr[mid - 1]) {
-                e = mid - 1; // Peak is to the left
-            } else {
-                s = mid + 1; // Peak is to the right
-            }
+        if (nums[mid] > nums[mid + 1]) { 
+            right = mid; // Peak is in the left half or at mid
+        } else { 
+            left = mid + 1; // Peak is in the right half
         }
-        return -1; // No peak found (shouldn't happen in a valid peak array)
     }
+    
+    return left; // `left` and `right` will converge to a peak
+}
 }
