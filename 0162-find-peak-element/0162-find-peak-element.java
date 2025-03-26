4c0530ca7cd1fcf788bdef1cd5cf66a8cc0ab1ba
Tanishaa5 <121441180+Tanishaa5@users.noreply.github.com>
2025-03-26 12:47:49 +0530

    Time: 0 ms (100%), Space: 42.7 MB (31.56%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,17 +1,43 @@
-class Solution{
-    public int findPeakElement(int[] nums) {
-    int left = 0, right = nums.length - 1;
+// class Solution{
+//     public int findPeakElement(int[] nums) {
+//     int left = 0, right = nums.length - 1;
     
-    while (left < right) {
-        int mid = left + (right - left) / 2;
+//     while (left < right) {
+//         int mid = left + (right - left) / 2;
 
-        if (nums[mid] > nums[mid + 1]) { 
-            right = mid; // Peak is in the left half or at mid
-        } else { 
-            left = mid + 1; // Peak is in the right half
-        }
-    }
+//         if (nums[mid] > nums[mid + 1]) { 
+//             right = mid; // Peak is in the left half or at mid
+//         } else { 
+//             left = mid + 1; // Peak is in the right half
+//         }
+//     }
     
-    return left; // `left` and `right` will converge to a peak
-}
+//     return left; // `left` and `right` will converge to a peak
+// }
+// }
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n=nums.length;
+        int s=0;
+        int e=n-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+           
+            
+                if ((mid == 0 || nums[mid] > nums[mid - 1]) && (mid == n - 1 || nums[mid] > nums[mid + 1]))
+
+                return mid; //ya to mid greater hain aage wale eleemnet se ya mid k aage koi element hi nhi hai mid last element h
+            
+            else if(nums[mid]<nums[mid+1]){
+                s=mid+1;
+
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        return -1;
+
+        
+    }
 }
